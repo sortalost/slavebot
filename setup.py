6ae974f2f080d4738af5dab4cc436cec8246f647
rickstaa <rick.staa@outlook.com>
2022-12-02 08:33:40 +0100

    feat: add hello world discord bot

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="discord_hello_world_bot",
+    version="1.0",
+    description="A simple discord bot that responds with 'Hello!' when you say 'hi'",
+    license="MIT",
+    author="Rick Staa",
+    author_email="rick.staa@outlook.com",
+    install_requires=["discord"],
+)
