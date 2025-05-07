e92b2fc85b403cd850209fc093bf44c3dc167942
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-05-07 22:15:42 +0530

    aiai

---
+++ src/bot.py
@@ -0,0 +1,98 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv("TOKEN")
+LOG = 877473404117209191
+prefix = "."
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or(prefix),
+    case_insensitive=True,
+    owner_id=(817359568945545226),
+    intents=intents, 
+    strip_after_prefix=True
+)
+
+lastmsg={}
+
+
+def gen_snipe(ctx,guild):
+	eb = []
+	try:
+		snipes = lastmsg[guild]
+	except KeyError:
+		lastmsg.update({guild:[]})
+		return []
+	if snipes==[]:
+		return []
+	i=0
+	for m in snipes:
+		i+=1
+		embed = discord.Embed(title=f"snipe {i}/{len(snipes)}")
+		embed.add_field(name="from", value=m.author.mention,inline=False)
+		embed.add_field(name="content",value=f"{m.content}\n",inline=False)
+		embed.add_field(name="in",value=m.channel.mention,inline=False)
+		embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
+		eb.append(embed)
+	return eb[::-1]
+
+
+@bot.event
+async def on_message_delete(message:discord.Message):
+	if message.author.bot==True:
+		return
+    try:
+		lastmsg[message.guild.id].append(message)
+	except:
+		lastmsg.update({message.guild.id:[]})
+		lastmsg[message.guild.id].append(message)
+	
+
+@bot.event
+async def on_ready():
+    print("up and running")
+    await bot.get_channel(LOG).send("running")
+
+
+@bot.event
+async def on_message(message):
+    # print("The message's content was", message.content)
+    if 'nigga' message.content.lower():
+        await message.reply("no u nigga")
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    em = discord.Embed(title="you broke me idiot", color=discord.Color.red(), timestamp=datetime.utcnow())
+    em.description = f"```{error}```"
+    await ctx.send(embed=em)
+
+
+@bot.command()
+async def ping(ctx):
+    '''
+    get latency
+    '''
+    await ctx.send(bot.latency)
+
+
+@bot.command()
+async def echo(ctx, *, content:str):
+    try:
+        ctx.message.delete()
+    except:
+        pass
+    await ctx.send(content)
+
+
+@bot.command(name="snipe",aliases=['snp'])
+async def snipe(ctx: commands.Context):
+	snp = self.gen_snipe(ctx,ctx.guild.id)
+	if snp==[]:
+		return await ctx.send("nothing to snipe", delete_after=5)
+	await discordSuperUtils.ButtonsPageManager(ctx, snp, button_color=3).run()
+
+
+print("running???")
+bot.run(TOKEN)  
+
