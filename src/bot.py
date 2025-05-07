134c801f3d99588a0611dee2d0dd4abe1e6c42d1
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-05-07 22:19:33 +0530

    yy

--- src/bot.py
+++ src/bot.py
@@ -1,52 +1,56 @@
 import os
 import discord
 from discord.ext import commands
+from datetime import datetime  # You were using datetime without importing
 
 TOKEN = os.getenv("TOKEN")
 LOG = 877473404117209191
 prefix = "."
+
+intents = discord.Intents.default()
+intents.message_content = True  # You'll likely need this for message access
+
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or(prefix),
     case_insensitive=True,
-    owner_id=(817359568945545226),
-    intents=intents, 
+    owner_id=817359568945545226,
+    intents=intents,
     strip_after_prefix=True
 )
 
-lastmsg={}
+lastmsg = {}
 
 
-def gen_snipe(ctx,guild):
-	eb = []
-	try:
-		snipes = lastmsg[guild]
-	except KeyError:
-		lastmsg.update({guild:[]})
-		return []
-	if snipes==[]:
-		return []
-	i=0
-	for m in snipes:
-		i+=1
-		embed = discord.Embed(title=f"snipe {i}/{len(snipes)}")
-		embed.add_field(name="from", value=m.author.mention,inline=False)
-		embed.add_field(name="content",value=f"{m.content}\n",inline=False)
-		embed.add_field(name="in",value=m.channel.mention,inline=False)
-		embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
-		eb.append(embed)
-	return eb[::-1]
+def gen_snipe(ctx, guild):
+    eb = []
+    try:
+        snipes = lastmsg[guild]
+    except KeyError:
+        lastmsg.update({guild: []})
+        return []
+    if snipes == []:
+        return []
+    i = 0
+    for m in snipes:
+        i += 1
+        embed = discord.Embed(title=f"snipe {i}/{len(snipes)}")
+        embed.add_field(name="from", value=m.author.mention, inline=False)
+        embed.add_field(name="content", value=f"{m.content}\n", inline=False)
+        embed.add_field(name="in", value=m.channel.mention, inline=False)
+        embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar.url)
+        eb.append(embed)
+    return eb[::-1]
 
 
 @bot.event
-async def on_message_delete(message:discord.Message):
-	if message.author.bot==True:
-		return
+async def on_message_delete(message: discord.Message):
+    if message.author.bot:
+        return
     try:
-		lastmsg[message.guild.id].append(message)
-	except:
-		lastmsg.update({message.guild.id:[]})
-		lastmsg[message.guild.id].append(message)
-	
+        lastmsg[message.guild.id].append(message)
+    except KeyError:
+        lastmsg.update({message.guild.id: [message]})
+
 
 @bot.event
 async def on_ready():
@@ -56,9 +60,9 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    # print("The message's content was", message.content)
-    if 'nigga' message.content.lower():
+    if 'nigga' in message.content.lower():  # Fixed missing 'in' keyword
         await message.reply("no u nigga")
+    await bot.process_commands(message)  # Needed or commands won't work
 
 
 @bot.event
@@ -70,29 +74,26 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    '''
-    get latency
-    '''
+    """Get latency"""
     await ctx.send(bot.latency)
 
 
 @bot.command()
-async def echo(ctx, *, content:str):
+async def echo(ctx, *, content: str):
     try:
-        ctx.message.delete()
+        await ctx.message.delete()
     except:
         pass
     await ctx.send(content)
 
 
-@bot.command(name="snipe",aliases=['snp'])
+@bot.command(name="snipe", aliases=['snp'])
 async def snipe(ctx: commands.Context):
-	snp = self.gen_snipe(ctx,ctx.guild.id)
-	if snp==[]:
-		return await ctx.send("nothing to snipe", delete_after=5)
-	await discordSuperUtils.ButtonsPageManager(ctx, snp, button_color=3).run()
+    snp = gen_snipe(ctx, ctx.guild.id)  # Removed 'self.'
+    if snp == []:
+        return await ctx.send("nothing to snipe", delete_after=5)
+    await discordSuperUtils.ButtonsPageManager(ctx, snp, button_color=3).run()
 
 
 print("running???")
-bot.run(TOKEN)  
-
+bot.run(TOKEN)
