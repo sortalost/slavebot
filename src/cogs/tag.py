924e6b3745eac98319a8c7e348d40415b81417e6
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-05-09 13:56:02 +0530

    ??

--- src/cogs/tag.py
+++ src/cogs/tag.py
@@ -1,4 +1,3 @@
-
 import asyncio
 import discord
 from discord.ext import commands
@@ -7,117 +6,85 @@ import json
 from discord.utils import get as g
 import difflib as dfb
 
-
 main_bank = "src/files/alltags.json"
 
-
 def load_all_tags():
-    with open(main_bank,'r') as f:
-        tags = json.load(f)
-    return tags
+    with open(main_bank, 'r') as f:
+        return json.load(f)
 
 def init_guild(guild):
-    tags=load_all_tags()
-    try:
-        return tags[str(guild)]
-    except KeyError:
-        tags.update(dict({str(guild):{}}))
-        with open(main_bank,'w') as f:
-            json.dump(tags,f,indent=4)
-        return True
-
-def new_tag(guild,tag,value,author):
     tags = load_all_tags()
-    tag=dict({tag.lower():{'value':value,'author':str(author)}})
-    tags[str(guild)].update(tag)
-    with open(main_bank,'w') as f:
-        json.dump(tags,f,indent=4)
+    if str(guild) not in tags:
+        tags[str(guild)] = {}
+        with open(main_bank, 'w') as f:
+            json.dump(tags, f, indent=4)
+    return tags[str(guild)]
+
+def new_tag(guild, tag, value, author):
+    tags = load_all_tags()
+    tags[str(guild)][tag.lower()] = {'value': value, 'author': str(author)}
+    with open(main_bank, 'w') as f:
+        json.dump(tags, f, indent=4)
     return True
 
-def rem_tag(guild,tag):
+def rem_tag(guild, tag):
     tags = load_all_tags()
-    tags[str(guild)].pop(tag.lower())
-    with open(main_bank,'w') as f:
-        json.dump(tags,f,indent=4)
+    tags[str(guild)].pop(tag.lower(), None)
+    with open(main_bank, 'w') as f:
+        json.dump(tags, f, indent=4)
     return True
 
-def get_tag(guild,tag):
-    users = load_all_tags()
-    bal = users[str(guild)][tag.lower()]
-    return bal
+def get_tag(guild, tag):
+    return load_all_tags()[str(guild)][tag.lower()]
 
 def get_guild_tags(guild):
-    tags=load_all_tags()
-    return dict(tags[str(guild)])
+    return dict(load_all_tags()[str(guild)])
 
-def is_tag(guild,tag):
-    try:
-        all_tags = load_all_tags()
-        all_tags[str(guild)][tag.lower()]
-        return True
-    except KeyError:
-        return False
-
+def is_tag(guild, tag):
+    return tag.lower() in load_all_tags().get(str(guild), {})
 
 class Tagging(commands.Cog):
-	"""Tag stuff"""
-	def __init__(self, bot:commands.Bot):
-		self.bot=bot
-		self.fp=main_bank
-		self.remote=database.DB(main=self.fp)
+    """Tag stuff"""
 
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.fp = main_bank
+        self.remote = database.DB(main=self.fp)
 
-	def disambiguate(self,tag,guild):
-		taglist = list(load_all_tags().get(str(guild)))
-		mt = dfb.get_close_matches(tag,taglist)
-		if mt==[]:
-			return ""
-		if len(mt)>3:
-			mt = mt[:3]
-		return f"Did you mean: {', '.join(mt)}"
-	
-	
-	@commands.Cog.listener()
-	async def on_ready(self):
-		start_tags = self.remote.get_remote_data()
-		with open(self.fp,'w') as f:
-			json.dump(start_tags, f, indent=4)
+    def disambiguate(self, tag, guild):
+        taglist = list(load_all_tags().get(str(guild), {}))
+        matches = dfb.get_close_matches(tag, taglist)
+        if matches:
+            return f"Did you mean: {', '.join(matches[:3])}"
+        return ""
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        start_tags = self.remote.get_remote_data()
+        with open(self.fp, 'w') as f:
+            json.dump(start_tags, f, indent=4)
 
-	@commands.command(aliases=['t'], help="tag the stuff you want")
-	async def tag(self,ctx,*,name=None):
-		if name is None:
-			return await ctx.invoke(self.bot.get_command("helptag"))
-		if name.lower().startswith("add"):
-			return await ctx.invoke(self.bot.get_command("addtag"),name=name[4:])
-		elif name.lower().startswith("del"):
-			return await ctx.invoke(self.bot.get_command("deltag"),name=name[4:])
-		elif name.lower().startswith("edit"):
-			return await ctx.invoke(self.bot.get_command("editag"),name=name[5:])
-		elif name.lower().startswith("info"):
-			return await ctx.invoke(self.bot.get_command("infotag"),name=name[5:])
-		elif name.lower().startswith("help"):
-			return await ctx.invoke(self.bot.get_command("helptag"))
-		elif name.lower().startswith("all"):
-			return await ctx.invoke(self.bot.get_command("all_tags"))
-		c = init_guild(ctx.guild.id)
-		if isinstance(c, bool):
-			return await ctx.send(f"tag manager initialized, try again.",delete_after=10)
-		try:
-			tag = get_tag(ctx.guild.id,name)
-			con = tag['value']
-		except KeyError:
-			return await ctx.send(f"no such tag exists\n{self.disambiguate(name,ctx.guild.id)}",delete_after=20)
-		u = g(ctx.guild.members,id=int(tag['author']))
-		await ctx.send(con)
-				
-	@commands.command(name="helptag")
-	async def helptag(self,ctx):
-		c = init_guild(ctx.guild.id)
-		if isinstance(c, bool):
-			return await ctx.send(f"tag manager initialiazed, try again.",delete_after=17)
-		eb=discord.Embed(title=f"TAGS {g(self.bot.emojis,name='tag')}")
-		eb.description=f"""\
+    @commands.group(invoke_without_command=True, aliases=['t'], help="tag the stuff you want")
+    async def tag(self, ctx, *, name=None):
+        if name is None:
+            return await ctx.invoke(self.bot.get_command("helptag"))
+
+        c = init_guild(ctx.guild.id)
+        if isinstance(c, bool):
+            return await ctx.send("Tag manager initialized, try again.", delete_after=10)
+
+        if not is_tag(ctx.guild.id, name):
+            return await ctx.send(f"No such tag exists.\n{self.disambiguate(name, ctx.guild.id)}", delete_after=20)
+
+        tag = get_tag(ctx.guild.id, name)
+        user = g(ctx.guild.members, id=int(tag['author']))
+        await ctx.send(tag['value'])
+
+    @tag.command(name="help")
+    async def helptag(self, ctx):
+        init_guild(ctx.guild.id)
+        eb = discord.Embed(title=f"TAGS {g(self.bot.emojis, name='tag')}")
+        eb.description = """
 **Usage:**
 ```fix
 .tag [TAG]
@@ -127,118 +94,89 @@ class Tagging(commands.Cog):
 .tag edit [TAG]
 .tag info [TAG]
 ```
-**and**,
-`add`, `del`, `edit`, `all`, `info` and `help` are reserved.
+**and**, `add`, `del`, `edit`, `all`, `info`, `help` are reserved.
 """
-		eb.set_footer(text=ctx.author.name,icon_url=ctx.author.avatar)
-		eb.timestamp=ctx.message.created_at
-		await ctx.send(embed=eb)
+        eb.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar)
+        eb.timestamp = ctx.message.created_at
+        await ctx.send(embed=eb)
 
-	@commands.command(name="addtag")
-	async def _add(self,ctx,*,name:str):
-		c = init_guild(ctx.guild.id)
-		if isinstance(c, bool):
-			return await ctx.send(f"tag manager initialized, try again.",delete_after=17)
-		try:
-			tag=get_tag(ctx.guild.id,name)
-			a=int(tag['author'])
-			await ctx.send(embed=discord.Embed(description=f"**tag \"{name}\", by <@{a}> already exists**"),delete_after=15)
-			return
-		except KeyError:
-			pass
-		def chk(m):return m.author==ctx.author and m.channel==ctx.channel
-		msg0=await ctx.send(f"1) What should the tag contain {ctx.author.mention}?? Send it")
-		value=await self.bot.wait_for("message",check=chk)
-		msg = await ctx.send(f"Creating tag...{g(self.bot.emojis,name='funUS')}")
-		new_tag(ctx.guild.id,name,value.content,ctx.author.id)
-		try:
-			await ctx.message.delete()
-			await value.delete()
-			await msg.delete()
-			await msg0.delete()
-		except:pass
-		self.remote.push_remote_data(load_all_tags())
-		await ctx.send(embed=discord.Embed(description=f"**{g(self.bot.emojis,name='tag')}tag\"{name.title()}\" by {ctx.author.mention} created{g(self.bot.emojis,name='tag')}**"))
-	
-	@commands.command(name="deltag")
-	async def _delete(self,ctx,*,name:str):
-		c = init_guild(ctx.guild.id)
-		if isinstance(c, bool):
-			return await ctx.send(f"tag manager initialized, try again",delete_after=17)
-		chk = is_tag(ctx.guild.id,name)
-		if chk is False:
-			await ctx.send("no such tag exists",delete_after=7)
-			return
-		tag = get_tag(ctx.guild.id,name)
-		if tag['author']!=str(ctx.author.id):
-			return await ctx.send("you dont own this tag",delete_after=7)
-		rem_tag(ctx.guild.id,name)
-		tag_num = len(get_guild_tags(ctx.guild.id))
-		self.remote.push_remote_data(load_all_tags())
-		await ctx.send(f"tag \"{name}\" has been successfully deleted\ntotal tags for this server: {tag_num}")
-	
-	
-	@commands.command(name="editag")
-	async def _edit(self,ctx,*,name:str):
-		c = init_guild(ctx.guild.id)
-		if isinstance(c, bool):
-			return await ctx.send(f"tag manager initialized, try again.",delete_after=17)
-		chk = is_tag(ctx.guild.id,name)
-		if chk is False:
-			await ctx.send("No Such Tag Exists!!!",delete_after=7)
-			return
-		tag = get_tag(ctx.guild.id,name)
-		if tag['author']!=str(ctx.author.id):
-			return await ctx.send("not yours...tsk...tsk",delete_after=7)
-		m1=await ctx.send(f"1) Any new name for the tag?? (send the same name if you want the same name, cuz same name's name is same xD)")
-		def chk(m):return m.author==ctx.author and m.channel==ctx.channel
-		nm=await self.bot.wait_for("message",check=chk)
-		if is_tag(ctx.guild.id,m1.content):
-			if get_tag(ctx.guild.id,m1.content)['author']!=str(ctx.author.id):
-				return await ctx.send("this tag already exists, sad for you. Send `.tag edit [TAG]` again",delete_after=15)
-		m2=await ctx.send(f"2) Any new content for the tag?? (send the same content if u want the same content, cuz same content's content is same xD)")
-		val=await self.bot.wait_for("message",check=chk)
-		rem_tag(ctx.guild.id,name)
-		new_tag(ctx.guild.id,nm.content,val.content,ctx.author.id)
-		try:
-			await ctx.message.delete()
-			await m1.delete()
-			await nm.delete()
-			await m2.delete()
-			await val.delete()
-		except:
-			pass
-		self.remote.push_remote_data(load_all_tags())
-		await ctx.send(embed=discord.Embed(title=f"tag edited.{g(self.bot.emojis,name='BlobBroke')}"))
+    @tag.command(name="add")
+    async def add_tag(self, ctx, *, name: str):
+        init_guild(ctx.guild.id)
+        if is_tag(ctx.guild.id, name):
+            author_id = int(get_tag(ctx.guild.id, name)['author'])
+            return await ctx.send(embed=discord.Embed(description=f"**Tag \"{name}\" by <@{author_id}> already exists**"), delete_after=15)
 
+        def check(m): return m.author == ctx.author and m.channel == ctx.channel
 
-	@commands.command(name="infotag")
-	async def _info(self,ctx,*,name:str):
-		c = init_guild(ctx.guild.id)
-		if isinstance(c, bool):
-			return await ctx.send(f"tag manager initialized, try again.",delete_after=17)
-		chk = is_tag(ctx.guild.id,name)
-		if chk is False:
-			await ctx.send("no such tag found",delete_after=7)
-			return
-		tag = get_tag(ctx.guild.id,name)
-		owner = await self.bot.fetch_user(tag['author'])
-		em = discord.Embed(colour=discord.Colour.random())
-		em.add_field(name="tag", value=name)
-		em.add_field(name="owner", value=owner.mention)
-		em.set_author(name=owner.name, icon_url=owner.avatar)
-		await ctx.send(embed=em)
+        await ctx.send(f"1) What should the tag contain {ctx.author.mention}?")
+        value = await self.bot.wait_for("message", check=check)
 
-	@commands.command()
-	async def all_tags(self,ctx):
-		c = init_guild(ctx.guild.id)
-		if isinstance(c, bool):return await ctx.send(f"tag manager initialized, try again.",delete_after=17)
-		tags=list(get_guild_tags(ctx.guild.id))
-		try:
-			await ctx.send(embed=discord.Embed(description="\n• ".join(tags)))
-		except:
-			await ctx.send(embed=discord.Embed(description=f"no tags {g(self.bot.emojis,name='shhAngryUS')}"))
+        new_tag(ctx.guild.id, name, value.content, ctx.author.id)
+        self.remote.push_remote_data(load_all_tags())
 
+        await ctx.send(embed=discord.Embed(description=f"**{g(self.bot.emojis, name='tag')} Tag \"{name.title()}\" by {ctx.author.mention} created!**"))
+
+    @tag.command(name="delete")
+    async def delete_tag(self, ctx, *, name: str):
+        init_guild(ctx.guild.id)
+        if not is_tag(ctx.guild.id, name):
+            return await ctx.send("No such tag exists", delete_after=7)
+
+        tag = get_tag(ctx.guild.id, name)
+        if tag['author'] != str(ctx.author.id):
+            return await ctx.send("You don't own this tag", delete_after=7)
+
+        rem_tag(ctx.guild.id, name)
+        self.remote.push_remote_data(load_all_tags())
+        await ctx.send(f"Tag \"{name}\" deleted. Remaining tags: {len(get_guild_tags(ctx.guild.id))}")
+
+    @tag.command(name="edit")
+    async def edit_tag(self, ctx, *, name: str):
+        init_guild(ctx.guild.id)
+        if not is_tag(ctx.guild.id, name):
+            return await ctx.send("No such tag exists", delete_after=7)
+
+        tag = get_tag(ctx.guild.id, name)
+        if tag['author'] != str(ctx.author.id):
+            return await ctx.send("You don't own this tag", delete_after=7)
+
+        def check(m): return m.author == ctx.author and m.channel == ctx.channel
+
+        await ctx.send("1) New name for the tag?")
+        new_name_msg = await self.bot.wait_for("message", check=check)
+
+        await ctx.send("2) New content for the tag?")
+        new_val_msg = await self.bot.wait_for("message", check=check)
+
+        rem_tag(ctx.guild.id, name)
+        new_tag(ctx.guild.id, new_name_msg.content, new_val_msg.content, ctx.author.id)
+        self.remote.push_remote_data(load_all_tags())
+
+        await ctx.send(embed=discord.Embed(title="Tag edited successfully."))
+
+    @tag.command(name="info")
+    async def info_tag(self, ctx, *, name: str):
+        init_guild(ctx.guild.id)
+        if not is_tag(ctx.guild.id, name):
+            return await ctx.send("No such tag", delete_after=7)
+
+        tag = get_tag(ctx.guild.id, name)
+        owner = await self.bot.fetch_user(tag['author'])
+        em = discord.Embed(color=discord.Color.random())
+        em.add_field(name="Tag", value=name)
+        em.add_field(name="Owner", value=owner.mention)
+        em.set_author(name=owner.name, icon_url=owner.avatar)
+        await ctx.send(embed=em)
+
+    @tag.command(name="all")
+    async def all_tags(self, ctx):
+        init_guild(ctx.guild.id)
+        tags = list(get_guild_tags(ctx.guild.id))
+        if tags:
+            await ctx.send(embed=discord.Embed(description="\n• " + "\n• ".join(tags)))
+        else:
+            await ctx.send(embed=discord.Embed(description=f"No tags {g(self.bot.emojis, name='shhAngryUS')}"))
 
 async def setup(bot):
-	await bot.add_cog(Tagging(bot))
+    await bot.add_cog(Tagging(bot))
