33916b6089d5628f700ed30040e51269b8b631b4
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-05-10 13:51:24 +0530

    sss

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -1,5 +1,5 @@
 
-def cleanup_code(self, content):
+def cleanup_code(content):
     """Automatically removes code blocks from the code."""
     if content.startswith('```') and content.endswith('```'):
         return '\n'.join(content.split('\n')[1:-1])
