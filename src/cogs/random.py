fe933a92d2dff6bb5c4b8c57f17d3d166d154fb7
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-05-17 19:48:11 +0530

    add csgocommand

---
+++ src/cogs/random.py
@@ -0,0 +1,28 @@
+import aiohttp
+from discord.ext import commands
+
+
+class Random(commands.Cog):
+    """Random commands"""
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(aliases=['cs'])
+    async def csgo(self, ctx, num:int = 1):
+        """get `n` videos from CSGOANI.ME"""
+        allurls = []
+        if int(num)>5:
+            return await ctx.send("`num` cannot be > 5")
+        for i in range(num):
+            async with aiohttp.ClientSession() as session:
+                async with session.post("https://csgoanime.vercel.app/new", json=payload, headers=headers) as response:
+                    if response.status != 200:
+                        i=1
+                        return await ctx.send("something went wrong...", delete_after=10)
+                    url = await response.json()
+                    allurls.append(url)
+        await ctx.send("\n".join(allurls))
+  
+
+async def setup(bot):
+    await bot.add_cog(Random(bot))
