48f8d31c64b6d81cf96d64e2a756d8952bb88210
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-05-17 20:17:19 +0530

    popo

--- src/cogs/random.py
+++ src/cogs/random.py
@@ -6,12 +6,13 @@ class Random(commands.Cog):
     """Random commands"""
     def __init__(self, bot):
         self.bot = bot
+        self.max_vid=5
     
     @commands.command(aliases=['cs'])
     async def csgo(self, ctx, num:int = 1):
         """get `n` videos from CSGOANI.ME"""
         allurls = []
-        if int(num)>5:
+        if int(num)>self.max_vid:
             return await ctx.send("`num` cannot be > 5")
         await ctx.typing()
         for i in range(num):
@@ -19,9 +20,21 @@ class Random(commands.Cog):
                 async with session.get("https://csgoanime.vercel.app/new") as response:
                     if response.status != 200:
                         return await ctx.send(response.status, delete_after=10)
-                    url = await response.json()
-                    allurls.append(url['video'])
+                    resp = await response.json()
+                    url = resp['video']
+                    name = url.split("/")[-1]
+                    allurls.append(f"`{i}`.[`{name}`]({url['video']})")
         await ctx.send("\n".join(allurls))
+
+
+    @commands.has_permissions(manage_messages=True)
+    @commands.command()
+    def maxcsgo(self,ctx,num:int=0):
+        if num==0:
+            return await ctx.send(f"Current CSGO videos threshold: `{self.max_vid}`")
+        self.max_vid=num
+        await ctx.send(f"Current CSGO videos threshold: `{self.max_vid}`")
+
   
 
 async def setup(bot):
