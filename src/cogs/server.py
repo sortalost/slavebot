6e8a1fb36464ee7462211bc9f63274a84a7bfb9a
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-05-18 17:49:25 +0530

    re

--- src/cogs/server.py
+++ src/cogs/server.py
@@ -2,6 +2,7 @@ import time
 import aiohttp
 import discord
 from discord.ext import commands
+from utils import database
 
 
 class Server(commands.Cog):
@@ -10,6 +11,7 @@ class Server(commands.Cog):
         self.bot = bot
         self.rulesch = 1373384320751894535
         self.roles = {'human':1373325900040634531}
+        self.remote = database.DB(main="wakewords.json")
     
     @commands.is_owner()
     @commands.command(aliases=['su'])
@@ -41,14 +43,21 @@ class Server(commands.Cog):
         await ch.send(rule)
         await ctx.reply(f"Check {ch.mention}")
 
-    
+
     @commands.Cog.listener()
-    async def on_member_join(self, member):
-        if member.bot:
-            return
-        member.guild.get_role(self.roles['human'])
-        await self.bot.get_channel(1373384320751894535).send(member.mention)
-        await member.add_roles(role)
+    async def on_message(self,msg):
+        guildwords = self.remote.get_remote_data()[msg.guild.id]
+        if msg.content.lower() in list(guildwords):
+            return await ctx.reply(guildwords[msg.content.lower()])
+    
+
+    @commands.command(aliases=['ww'])
+    async def wakeword(self,ctx,word:str,*,reply:str):
+        words = self.remote.get_remote_data()
+        words[ctx.guild.id][word] = reply
+        self.remote.push_remote_data(words)
+        await ctx.send(f"Added `{word}` amongst:\n{', '.join(list(words))}")
+
 
 async def setup(bot):
     await bot.add_cog(Server(bot))
