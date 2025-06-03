cc89adfa5fcaba74d4c855fe01a15474f56a769b
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-06-03 20:51:34 +0530

    double rpelies?

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import threading
 import asyncio
 from src.web import app
-from src.bot import bot
+from src.bot import bot_core
 
 def run_flask():
     app.app.run(host="0.0.0.0", port=3000) # T_T
 
 if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
-    asyncio.run(bot.run_async())
+    asyncio.run(bot_core.run_async())
